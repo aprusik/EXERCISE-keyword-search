f05c9ca10027e3df09e2f7a7b029f86e668a3cbe
Austin Prusik <29558544+aprusik@users.noreply.github.com>
2019-09-19 20:16:44 -0400

    Read lines from StdIn, parse into sentences, print sentences

--- src/main/scala/KeywordSearch.scala
+++ src/main/scala/KeywordSearch.scala
@@ -1,3 +1,27 @@
-object KeywordSearch {
+import scala.collection.mutable
+import scala.io.Source
 
+object KeywordSearch extends App {
+  val sentence = new StringBuilder()
+  var sentenceList: mutable.Queue[String] = mutable.Queue()
+
+  for (line <- Source.stdin.getLines()) {
+    val ln = line.trim + " "
+    var end = ln.indexOfSlice(". ")
+    if (end == -1) sentence ++= ln
+    else {
+      while (end != -1) {
+        sentence ++= ln.slice(0, end)
+        sentenceList += sentence.result() + "."
+        sentence.clear()
+
+        val prevEnd = end
+        end = ln.indexOfSlice(". ", end + 1)
+        if (end == -1) sentence ++= ln.slice(prevEnd+2, ln.length)
+        else sentence ++= ln.slice(prevEnd + 2, end)
+      }
+    }
+  }
+
+  sentenceList.foreach(println)
 }
